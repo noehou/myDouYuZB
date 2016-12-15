57144ad13d429a89467a7675e19122f97a4280db
noehou <750871487@qq.com>
2016-12-15 13:04:41 +0800

    娱乐界面

--- DYZB/DYZB/Classes/Home/Controller/AmuseViewController.swift
+++ DYZB/DYZB/Classes/Home/Controller/AmuseViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
-
+private let kMenuViewH : CGFloat = 200
 
 class AmuseViewController: BaseAnchorViewController {
     //MARK：懒加载属性
     fileprivate lazy var amuseVM : AmuseViewModel = AmuseViewModel()
+    fileprivate lazy var menuView : AmuseMenuView = {
+       let menuView = AmuseMenuView.amuseMenuView()
+        menuView.frame = CGRect(x: 0, y: -kMenuViewH, width: kScreenW, height: kMenuViewH)
+        return menuView
+    }()
 }
-
+//MARK：-设置UI界面
+extension AmuseViewController {
+    override func setupUI() {
+        super.setupUI()
+        
+        //将菜单的view添加到控制器的view
+        collectionView.addSubview(menuView)
+        collectionView.contentInset = UIEdgeInsets(top: kMenuViewH, left: 0, bottom: 0, right: 0)
+    }
+}
 
 //MARK:-请求数据
 extension AmuseViewController {
@@ -23,6 +37,9 @@ extension AmuseViewController {
         //2、请求数据
         amuseVM.loadAmuseData {
             self.collectionView.reloadData()
+            var tempGroups = self.amuseVM.anchorGroups
+            tempGroups.removeFirst()
+            self.menuView.groups = tempGroups
         }
     }
 }
---
+++ DYZB/DYZB/Classes/Home/View/Cell/AmuseMenuViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  AmuseMenuViewCell.swift
+//  DYZB
+//
+//  Created by Tommaso on 2016/12/15.
+//  Copyright © 2016年 Tommaso. All rights reserved.
+//
+
+import UIKit
+
+private let kGameCellID = "kGameCellID"
+
+class AmuseMenuViewCell: UICollectionViewCell {
+    //MARK：数组模型
+    var groups : [AnchorGroup]? {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    
+    //MARK:控件属性
+    @IBOutlet weak var collectionView: UICollectionView!
+    //MARK:从xib中加载
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        collectionView.register(UINib(nibName: "CollectionGameCell", bundle: nil), forCellWithReuseIdentifier: kGameCellID)
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let itemW = collectionView.bounds.width / 4
+        let itemH = collectionView.bounds.height / 2
+        layout.itemSize = CGSize(width: itemW, height: itemH)
+    }
+}
+
+extension AmuseMenuViewCell : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return groups?.count ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // 1、求出cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kGameCellID, for: indexPath) as! CollectionGameCell
+        
+        // 2、给cell设置数据
+        cell.baseGame = groups![indexPath.item]
+        cell.clipsToBounds = true
+        return cell
+    }
+}
